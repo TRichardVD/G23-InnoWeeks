f8deb7e391e95d70435e5310efbdf32f51b19543
TRichardVD <150446958+TRichardVD@users.noreply.github.com>
2024-06-07 16:34:44 +0200

    update

---
+++ Backend/test/test/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    internal class Program
+    {
+
+
+
+        public static string RemoveAccents(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return input;
+            }
+
+            // Remplacer les caractères accentués par leurs équivalents non accentués
+            string[] accents = new string[] { "áàâäãåą", "éèêëę", "íìîïı", "óòôöõő", "úùûüŭů", "çćč", "ñń", "ýÿ", "žźż" };
+            string[] replacements = new string[] { "a", "e", "i", "o", "u", "c", "n", "y", "z" };
+
+            for (int i = 0; i < accents.Length; i++)
+            {
+                foreach (char accent in accents[i])
+                {
+                    input = input.Replace(accent, replacements[i][0]);
+                }
+            }
+
+            return input;
+        }
+
+        public object
+
+
+
+
+
+        static void Main(string[] args)
+        {   
+
+
+
+
+
+
+            Console.WriteLine("Hello, \n\n");
+            while (true)
+            {
+                Console.Write("Votre Texte : ");
+                string[] tab_msg = RemoveAccents(Console.ReadLine()).Replace(@"[^a-zA-Z0-9]", "").Split(' '); ;
+
+                Console.WriteLine($"Texte corrigé : {tab_msg}");
+                
+
+                // Extraction des données
+
+                foreach (string word in tab_msg)
+                {
+                    
+                    
+
+
+
+
+
+
+
+                }
+
+
+
+
+
+
+
+            }
+        }
+
+    }
+}
--- Database/maladieDB/ScriptCreation/CreateDBMaladie.sql
+++ Database/maladieDB/ScriptCreation/CreateDBMaladie.sql
@@ -1,8 +1,8 @@
 
-    DROP DATABASE db_maladie;
+DROP DATABASE db_maladie;
 
-    CREATE DATABASE db_maladie CHARACTER SET utf8 COLLATE utf8_general_ci;
-    USE db_maladie
+CREATE DATABASE db_maladie CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE db_maladie
 
    CREATE TABLE t_cause(
    idCause INT AUTO_INCREMENT,
@@ -19,7 +19,33 @@ CREATE TABLE t_symptome(
 CREATE TABLE t_medicament(
    idMedicament INT AUTO_INCREMENT,
    medNom VARCHAR(50) NOT NULL,
+   medType ENUM('en vente libre', 'prescription') NOT NULL DEFAULT 'prescription',
    PRIMARY KEY(idMedicament)
+);
+
+CREATE TABLE t_conseil(
+   idConseil INT AUTO_INCREMENT,
+   conType ENUM('médical', 'naturel', 'activité') NOT NULL,
+   conTexte VARCHAR(500),
+   PRIMARY KEY(idConseil)
+);
+
+CREATE TABLE t_partieDuCorps(
+   idPartieDuCorps INT AUTO_INCREMENT,
+   parNom VARCHAR(50),
+   PRIMARY KEY(idPartieDuCorps)
+);
+
+CREATE TABLE t_synonymeCause(
+   idSynonymeCause INT AUTO_INCREMENT,
+   synTexte VARCHAR(255),
+   PRIMARY KEY(idSynonymeCause)
+);
+
+CREATE TABLE t_synCauses(
+   idSynCauses INT AUTO_INCREMENT,
+   synTexte VARCHAR(255),
+   PRIMARY KEY(idSynCauses)
 );
 
 CREATE TABLE t_causer(
@@ -47,4 +73,34 @@ CREATE TABLE t_soulage(
    FOREIGN KEY(idMedicament) REFERENCES t_medicament(idMedicament)
 );
 
+CREATE TABLE t_soulager(
+   idSymptome INT,
+   idConseil INT,
+   PRIMARY KEY(idSymptome, idConseil),
+   FOREIGN KEY(idSymptome) REFERENCES t_symptome(idSymptome),
+   FOREIGN KEY(idConseil) REFERENCES t_conseil(idConseil)
+);
 
+CREATE TABLE t_etreSynonymeSymp(
+   idSymptome INT,
+   idSynonymeCause INT,
+   PRIMARY KEY(idSymptome, idSynonymeCause),
+   FOREIGN KEY(idSymptome) REFERENCES t_symptome(idSymptome),
+   FOREIGN KEY(idSynonymeCause) REFERENCES t_synonymeCause(idSynonymeCause)
+);
+
+CREATE TABLE t_etreSynonymeCause(
+   idCause INT,
+   idSynCauses INT,
+   PRIMARY KEY(idCause, idSynCauses),
+   FOREIGN KEY(idCause) REFERENCES t_cause(idCause),
+   FOREIGN KEY(idSynCauses) REFERENCES t_synCauses(idSynCauses)
+);
+
+CREATE TABLE t_etreSynonyme(
+   idPartieDuCorps INT,
+   idPartieDuCorps_1 INT,
+   PRIMARY KEY(idPartieDuCorps, idPartieDuCorps_1),
+   FOREIGN KEY(idPartieDuCorps) REFERENCES t_partieDuCorps(idPartieDuCorps),
+   FOREIGN KEY(idPartieDuCorps_1) REFERENCES t_partieDuCorps(idPartieDuCorps)
+);
